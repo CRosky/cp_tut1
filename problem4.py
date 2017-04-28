553e4ba5e4b244d29c2999dc7c260194ad5348c8
CRosky <2013046483@ufs4life.ac.za>
2017-04-28 11:45:56 +0200

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -9,7 +9,7 @@ Created on Thu Apr 27 13:55:41 2017
 import numpy as np
 x = np.arange(11)
 odd = x[1:-2:2]
-odd = np.delete(x[1::], even)
+odd = np.delete(x[1:-2], even)
 
 print('A lsit of odd points ' + repr(odd))
 print('A lsit of even points ' + repr(even))
